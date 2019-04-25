94587a842ec28ae02958cc0fd04101198f57d796
EsGeh <SamuelGfroerer@googlemail.com>
2019-04-25 12:03:43 +0200

    sgDevice uses "modes". added sdKeyboard objs, which works similar.

--- dependencies.fish
+++ dependencies.fish
@@ -24,5 +24,5 @@ set ttymidi \
 set structuredData \
 	structuredData \
 	https://github.com/EsGeh/structuredData.git \
-	2f712db19abbc071a40c2d102cdaef0c22a1790a \
+	ff7e67f8c8a29e53c06bde649b8c4c1e7cb682fb \
 	"scripts/init.fish --deps-dir \$DEP_DIR"
