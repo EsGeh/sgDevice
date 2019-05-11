e3209dd269faa61a9348dfa84caa86cd0c91830c
EsGeh <SamuelGfroerer@googlemail.com>
2019-05-12 00:34:45 +0200

    fix test script to read from usb: should work in hex or ascii mode now!

--- scripts/arduino_usbdump.fish
+++ scripts/arduino_usbdump.fish
@@ -56,13 +56,13 @@ if set -q _flag_h
 	exit 0
 else
 	if set -q _flag_mode
-		set mode (readlink -m $_flag_mode)
+		set mode $_flag_mode
 	end
 	if set -q _flag_dev
 		set usb_dev (readlink -m $_flag_dev)
 	end
 	if set -q _flag_baud
-		set baud_rate (readlink -m $_flag_baud)
+		set baud_rate $_flag_baud
 	end
 end
 
@@ -73,12 +73,20 @@ stty -F $usb_dev $baud_rate
 stty -a -F $usb_dev
 
 ## skip arduino internal protocol messages:
+echo "wait a moment..."
 sleep 3
 
-if test $mode = 'ascii'
-	tail -f $usb_dev
+echo "ready!"
+echo "mode: $mode"
+
+if test "$mode" = 'ascii'
+	cat $usb_dev
+	# tail -f $usb_dev
 else
 	# dump as hex:
+	# run picocom to set necessary params for usb device
+	# (especially: disable line buffering):
+	picocom --noreset --exit $usb_dev > /dev/null
 	cat $usb_dev | hexdump -v -e '1/1 "x%02X\n"'
 	# tail -f $usb_dev | hexdump -v -e '1/1 "x%02X\n"'
 end
