46bfd265bf84874e9e49988f8d72eb8eafc32237
EsGeh <SamuelGfroerer@googlemail.com>
2019-06-22 23:31:31 +0200

    send all controller values, updated schematic (corresp. to hardware)

--- src/arduino/sgDevice_ver2/sgDevice_ver2.ino
+++ src/arduino/sgDevice_ver2/sgDevice_ver2.ino
@@ -2,19 +2,8 @@
 
 /*
 	NOTES:
-		Messages from MIDI-IN are directly sent to the pc.
-		Messages from PC are directly sent to MIDI-OUT.
-		Control changes are transmitted to the pc via SysEx Messages
-		
-				0xf0: //start sysex Message
-				<control id>
-				<value>
-				0xf7 //end sysex message
-
-		format for the triggers:
-				0xf0: //start sysex Message
-				<control id>
-				0xf7 //end sysex message
+	Messages from MIDI-IN are directly sent to the pc.
+	Messages from PC are directly sent to MIDI-OUT.
 */
 
 //#define DEBUG_PRINT
@@ -28,13 +17,12 @@ class Analog
 	private:
 		float analog[ANALOG_COUNT];
 	public:
-
 		Analog()
 		{
 			for(int i=0; i<ANALOG_COUNT; i++)
 			{
-				//analog[i] = 0;
-				analog[i]= ((float )analogRead(i)) / ((float ) ANALOG_PIN_RESOULUTION);
+			//analog[i] = 0;
+			analog[i]= ((float )analogRead(i)) / ((float ) ANALOG_PIN_RESOULUTION);
 			}
 		};
 
@@ -89,55 +77,126 @@ class Analog
 				#endif
 			#endif
 		}
+};
 
+class Buttons
+{
+	// values
+	private:
+		unsigned int pin0;
+		unsigned int count;
+		unsigned int control_id;
+	public:
+		boolean* value;
+	// methods:
+	public:
+		Buttons(
+			const unsigned int pin0,
+			const unsigned int count,
+			const unsigned int control_id
+		)
+			:pin0(pin0),
+			count(count),
+			control_id(control_id),
+			value(NULL)
+		{
+			value = new boolean[count];
+			for (int i=0; i<count ; i++)
+			{
+				value[i]=false;
+			}
+		}
+		~Buttons() {
+			delete[] value;
+		}
+		void setup()
+		{
+			for( int i=0; i<count; i++ )
+			{
+				pinMode(pin0+i, INPUT_PULLUP);
+			}
+		}
+		// overwrite:
+		void OnChange(const unsigned int index, boolean newValue) { }
+		void update()
+		{
+			for (int i=0; i< count ; i++)
+			{
+				if(digitalRead(pin0+i)!= value[i])
+				{
+					value[i]=!value[i];
+					#ifdef DEBUG_PRINT
+						Serial.println(String("Buttons: pin") + String(pin0+i) + String("] = " + String((unsigned int )value[i])));
+					#else
+						Serial.write( 0xB0 );
+						Serial.write( control_id + i );
+						Serial.write((byte ) value[i]);
+					#endif
+				}
+			}
+		}
 };
 
 class Midi
 {
 	private:
 	public:
-		Midi()
-		{
-		}
+	Midi()
+	{
+	}
 
-		void setup()
-		{
-		}
+	void setup()
+	{
+	}
 
-		// MIDI-In -> PC
-		void update_midi_in()
-		{
-			int input = -1;
-			if( Serial1.available() > 0 ) {
-				input = Serial1.read();
-				// filter out "timing" and "Active Sensing" input:
-				if( input != 0b11111000 && input != 0b11111110 ) {
-					#ifdef DEBUG_PRINT
-						//Serial.println( analog[i] );
-						Serial.print( "MIDI->PC: " );
-						Serial.println( input, HEX);
-					#else
-						Serial.write( (byte )input );
-						Serial.flush();
-					#endif
-				}
-			}
+	// MIDI-In -> PC
+	void update_midi_in()
+	{
+		int input = -1;
+		if( Serial1.available() > 0 ) {
+		input = Serial1.read();
+		// filter out "timing" and "Active Sensing" input:
+		if( input != 0b11111000 && input != 0b11111110 ) {
+			#ifdef DEBUG_PRINT
+			//Serial.println( analog[i] );
+			Serial.print( "MIDI->PC: " );
+			Serial.println( input, HEX);
+			#else
+			Serial.write( (byte )input );
+			Serial.flush();
+			#endif
 		}
-
-		// PC -> MIDI-Out
-		void update_midi_out()
-		{
-			int input = -1;
-			if( Serial.available() > 0 ) {
-				input = Serial.read();
-				Serial1.write( input );
-			}
 		}
+	}
 
+	// PC -> MIDI-Out
+	void update_midi_out()
+	{
+		int input = -1;
+		if( Serial.available() > 0 ) {
+		input = Serial.read();
+		Serial1.write( input );
+		}
+	}
 };
 
 Analog analog;
 Midi midi;
+Buttons switches(
+	PIN_SWITCH,
+	SWITCHES_COUNT,
+	SWITCHES_CONTROL_ID
+);
+Buttons triggers(
+	PIN_TRIGGERS,
+	TRIGGERS_COUNT,
+	TRIGGERS_CONTROL_ID
+);
+Buttons meta(
+	PIN_META,
+	META_COUNT,
+	META_CONTROL_ID
+);
 
 void setup() {
 	Serial.begin(PC_RATE);
@@ -147,6 +206,9 @@ void setup() {
 
 	analog.setup();
 	midi.setup();
+	switches.setup();
+	triggers.setup();
+	meta.setup();
 
 	#ifdef DEBUG_PRINT
 	Serial.println("initializing");
@@ -156,7 +218,10 @@ void setup() {
 //Message
 
 void loop() {
-	//analog.update();
+	analog.update();
 	midi.update_midi_in();
 	midi.update_midi_out();
+	switches.update();
+	triggers.update();
+	meta.update();
 }
--- src/global/Constants_ver2.h
+++ src/global/Constants_ver2.h
@@ -13,19 +13,45 @@
 /////////////////////////////
 
 #define ANALOG_CONTROL_ID 0
+#define ANALOG_COUNT 16
+
+#define SWITCHES_CONTROL_ID 16
+#define SWITCHES_COUNT 16
+
+/* midi control numbers 32..63:
+ if ANALOG_DOUBLE_PRECISION is defined,
+ these may specify the
+ least significant bits (LSB) of
+ the controls 0..31.
+ This way 2 midi messages are grouped
+ for higher precision.
+*/
+
+#define TRIGGERS_CONTROL_ID 64
+#define TRIGGERS_COUNT 8
+
+#define META_CONTROL_ID 96
+#define META_COUNT 4
+
+/////////////////////////////
+// arduino pins
+/////////////////////////////
+
+#define PIN_SWITCH 22
+#define PIN_TRIGGERS 38
+#define PIN_META 46
 
 /////////////////////////////
 // analog parameters:
 /////////////////////////////
 
-#define ANALOG_COUNT 1
 #define ANALOG_MIDI_RES 127.0
 
 #define ANALOG_STEPS ANALOG_MIDI_RES
 
-// if defined: use 2 midi messages to use 14 bytes instead of 7 bytes
-// define this, if you want more than ANALOG_MIDI_RES > 127.0
+// if defined: use 2 midi messages to use 14 bytes instead of 7 bytes.
 
-#define ANALOG_DOUBLE_PRECISION 0
+// uncomment, if you want more than ANALOG_MIDI_RES > 127.0:
+//#define ANALOG_DOUBLE_PRECISION
 
 #endif
